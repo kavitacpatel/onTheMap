cf195d5880c8d7c582246b4712a5962212c575f3
kavita patel <kavita_cpatel@yahoo.com>
2016-04-26 20:51:17 -0400

    adding files

--- udacityClient.swift
+++ udacityClient.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class udacityClient
+{
+    
+    struct Client {
+       static var first = ""
+       static var last = ""
+       static var latitude = 0.0
+       static var longitude = 0.0
+       static var mapString = ""
+       static var mediaURL = ""
+       static var objectId = ""
+       static var uniqueKey = ""
+        }
+    func clientDict(first:String, last: String, latitude: Double, longitude: Double, mapstring: String, mediaurl: String, objectid: String, uniqueid: String)-> NSDictionary
+    {
+        
+        let returnDict:NSDictionary? = [
+            "firstName": first,
+            "lastName": last,
+            "latitude": latitude,
+            "longitude": longitude,
+            "mapString": mapstring,
+            "mediaURL": mediaurl,
+            "objectId": objectid,
+            "uniqueKey": uniqueid
+        ];
+        return returnDict!
+    }
+}
